1bc24cbc7ba76071437b45dda1821b1513db5433
camellia98 <2927375411@qq.com>
2023-12-03 22:38:56 +0800

    学习vue脚手架

---
+++ Vue/测试/App.vue
@@ -0,0 +1,24 @@
+<template>
+  <!-- 方便方式一个<V就可以快速显示Vue -->
+    <!-- 第三步 使用 -->
+    <div>
+        <Tsest></Tsest>
+    </div>
+</template>
+
+<script>
+    // 第一步 引入
+    import  Test from './Test.vue'   //import引入
+    export default {
+        name:"APP",//App汇总所有有的组件
+
+        //第二步 注册组件
+        components:{
+           Test
+        }
+    }
+</script>
+
+<style>
+
+</style>
---
+++ Vue/测试/Test.vue
@@ -0,0 +1,85 @@
+<template>
+    <table>
+
+    </table>
+    <tbody>
+        <div id="root">
+            <div class="my-body">
+                <div class="my-title">{{title}}</div>
+                <input class="my-input" v-model="title" placeholder="开心"/><br><br>
+                <button @click="clearText()" class="my-button">清空</button>
+            </div>
+        </div>
+    </tbody>
+    <tfoot>
+        
+    </tfoot>
+   
+</template>
+
+<script>
+     export default{
+        data: {
+            title: '',
+        },
+        beforeCreate() {
+           alert('第二次测试')
+        },
+        methods: {
+            clear() {
+                this.title = ''
+            },
+            clearText(){
+                this.title = ''
+            }
+        },
+     }
+</script>
+
+<style>
+ * {
+            margin: 0;
+            padding: 0;
+            box-sizing: border-box;
+            height: 100%;
+            width: 100%;
+        }
+        .my-body {
+            height: 100%;
+            width: 100%;
+            background-image: url(./1.jpg);
+            text-align: center;
+            margin: 0 auto;
+            padding-top: 200px;
+        }
+
+        .my-input {
+            height: 32px;
+            width: 200px;
+            opacity: 40%;
+            border-radius: 10px;
+            border: 1px solid gray;
+        }
+
+        .my-title {
+            height: 50px;
+            text-align: center;
+            font-size: 48px;
+            margin: 10px;
+        }
+
+        .my-button {
+            height: 30px;
+            width: 100px;
+            opacity: 40%;
+            border-radius: 10px;
+            border: 1px solid gray;
+            background-color: #fff;
+        }
+        .my-button:hover {
+            background: rebeccapurple;
+        }
+        .my-button:active{
+            background-color: #fff;
+        }
+</style>
